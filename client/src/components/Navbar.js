b720757ccb8c9a3a003b12688531c788b7e3fe49
Auriane Viggiano <auriane.vigg@gmail.com>
2022-02-13 21:29:32 +0100

    debut css nav

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,14 +4,16 @@ import { Link } from 'react-router-dom';
 const navbar = () => {
   return (
     <nav className="nav-style">
-      <ul>
-        <li><Link to='/homepage'>Home</Link></li>
-        <li><Link to='/destinations'> Idées de Destinations </Link></li>
-        <li><Link to='/favoris'> Mes Destinations </Link></li>
-        <li><Link to='/user'> Mon Compte </Link></li>
-        <li><Link to='/signup'> Signup </Link></li>
-        <li><Link to='/login'> Login </Link></li>
-      </ul>
+        <ul class="nav1">
+          <li><Link to='/homepage'>Home</Link></li>
+          <li><Link to='/destinations'> Idées de Destinations </Link></li>
+          <li><Link to='/favoris'> Mes Destinations </Link></li>
+          <li><Link to='/user'> Mon Compte </Link></li>
+        </ul>
+        <ul class="nav2">
+          <li><Link to='/signup'> Signup </Link></li>
+          <li><Link to='/login'> Login </Link></li>
+        </ul>
     </nav>
   )
 }
